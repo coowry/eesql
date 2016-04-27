566ca29a3f0e37090afade4b14e6d442969c1b24
Ángel Herranz <aherranz@fi.upm.es>
2016-04-27 04:06:11 +0200

    Refactoring: introducing non terminal symbols from SQL grammar.

--- include/eesql.hrl
+++ include/eesql.hrl
@@ -10,13 +10,10 @@
 -record(
    select,
    {
-     dup = all :: eesql:dup(),
-     columns = [] :: list(eesql:column()), %% List of columns to retrieve, * as []
+     quantifier = all :: eesql:set_quant(),
+     columns = [] :: list(eesql:derived_column()), %% List of columns to retrieve, * as []
      from :: eesql:from_clause(), %% (FROM) Source of data, table names for the moment
-     where = [] :: list(eesql:predicate()), %% (WHERE) search_condition
-     group_by = [] :: list(eesql:group_by_expr()), %% (GROUP BY) group_by_expression
-     having = [] :: list(eesql:predicate()), %% (HAVING) search_condition
-     order_by = [] :: list(eesql:order_by_expr()) %% (ORDER BY) order_expression [ASC | DESC]
+     where :: eesql:predicate() %% (WHERE) search condition
    }
   ).
 
@@ -33,25 +30,25 @@
 -record(
    insert,
    {
-     table :: eesql:table_ref(),
-     columns :: nonempty_list(eesql:column()), %% List of columns to set
-     values :: nonempty_list(nonempty_list(eesql:value())) %% (VALUES) values to insert
+     table :: eesql:table_name(),
+     columns :: nonempty_list(eesql:column_name()), %% List of columns to set
+     values :: nonempty_list(eesql:row_value_expr()) %% (VALUES) values to insert
    }
   ).
 
 -record(
    update,
    {
-     table :: eesql:table_ref(),
-     set :: nonempty_list({eesql:name(), eesql:expr()}), %% (SET) columns to update
-     where = [] :: list(eesql:predicate()) %% (WHERE) search_condition
+     table :: eesql:table_name(), %% UPDATE target table
+     set :: nonempty_list(eesql:set_clause()), %% (SET) columns to update
+     where :: eesql:predicate() %% (WHERE) search condition
    }
   ).
 
 -record(
    delete,
    {
-     from :: eesql:table_ref(), %% (FROM) Source of data, table names for the moment
-     where = [] :: list(eesql:predicate()) %% (WHERE) search_condition
+     table :: eesql:table_name(), %% (DELETE FROM) target table
+     where :: eesql:predicate() %% (WHERE) search_condition
    }
   ).
--- test/eesql_tests.erl
+++ test/eesql_tests.erl
@@ -5,12 +5,12 @@
 
 select1_test() ->
   Select_AST = eesql:to_sql(#select{from = [users]}),
-  ?assertEqual("SELECT ALL * FROM users ;",
+  ?assertEqual("SELECT ALL * FROM users;",
                lists:flatten(io_lib:format("~s",[Select_AST]))).
 
 select2_test() ->
   Select_AST = eesql:to_sql(#select{columns=[username, name],from = [users]}),
-  ?assertEqual("SELECT ALL username, name FROM users ;",
+  ?assertEqual("SELECT ALL username, name FROM users;",
                lists:flatten(io_lib:format("~s",[Select_AST]))).
 
 select_join_test() ->
@@ -20,25 +20,40 @@ select_join_test() ->
                                              left = users,
                                              right = emails,
                                              spec = {'users.id','=','emails.id'}}]}),
-  ?assertEqual("SELECT ALL users.name, emails.address FROM users INNER JOIN emails ON users.id = emails.id ;",
+  ?assertEqual("SELECT ALL users.name, emails.address FROM users INNER JOIN emails ON users.id = emails.id;",
                lists:flatten(io_lib:format("~s",[Select_AST]))).
 
 select_theta_test() ->
   Select_AST = eesql:to_sql(#select{
                                columns=['users.name','emails.address'],
                                from = [users,emails],
-                               where=[{'users.id','=','emails.id'}]}),
+                               where= {'users.id','=','emails.id'}
+                              }),
   ?assertEqual("SELECT ALL users.name, emails.address FROM users, emails WHERE users.id = emails.id;",
                lists:flatten(io_lib:format("~s",[Select_AST]))).
 
 select_gt_test() ->
   Select_AST = eesql:to_sql(#select{
                                from = [users],
-                               where=[{created, '>', 1459286860}]}),
+                               where= {created, '>', 1459286860}
+                              }),
   ?assertEqual("SELECT ALL * FROM users WHERE created > 1459286860;",
                lists:flatten(io_lib:format("~s",[Select_AST]))).
 
 delete_test() ->
-  Delete_AST = eesql:to_sql(#delete{from=preuser}),
+  Delete_AST = eesql:to_sql(#delete{table = preuser}),
   ?assertEqual("DELETE FROM preuser RETURNING *;",
                lists:flatten(io_lib:format("~s",[Delete_AST]))).
+
+insert_test() ->
+  Insert_AST = eesql:to_sql(#insert{table = preuser,
+                                    columns = [c1, c2],
+                                    values = [[<<"a">>,21],[<<"b">>,42]]}),
+  ?assertEqual("INSERT INTO preuser (c1, c2) VALUES ('a', 21), ('b', 42) RETURNING *;",
+               lists:flatten(io_lib:format("~s",[Insert_AST]))).
+
+update_test() ->
+  Update_AST = eesql:to_sql(#update{table = preuser,
+                                    set = [{a,21}]}),
+  ?assertEqual("UPDATE preuser SET a = 21 RETURNING *;",
+               lists:flatten(io_lib:format("~s",[Update_AST]))).
